032545fed936ee7b840a6a40e82251833f85b561
Lance Lafontaine <lance.lafontaine92@gmail.com>
2015-02-17 17:12:16 -0500

    Adds mp3 theme. Fixes audio tag sources. Removes additional html div.

--- pokemon.js
+++ pokemon.js
@@ -2,18 +2,17 @@ var randomNumber = Math.floor(Math.random()* (152 - 1)) + 1;
 var i;
 
 function playSound(soundfile) {
-	document.getElementById("soundline1").innerHTML = '<audio autoplay="autoplay" hidden="hidden"><source src="'+soundfile+'.mp3" /><source src="'+soundfile+'.ogg" /><source src="'+soundfile+'.mp3.wav" /></audio>';
-
+	document.getElementById("soundline1").innerHTML = '<audio src="'+soundfile+'.mp3" autoplay="autoplay" hidden="hidden"></audio>';
 }
 
 function randomCry() {
-	document.getElementById("soundline1").innerHTML = '<audio autoplay="autoplay" hidden="hidden"><source src="'+randomNumber+'.mp3" /><source src="'+randomNumber+'.ogg" /><source src="'+randomNumber+'.mp3.wav" /></audio>';
+	document.getElementById("soundline1").innerHTML = '<audio src="'+randomNumber+'.mp3" autoplay="autoplay" hidden="hidden"></audio>';
 }
 
 function theme() {
 	if (document.getElementById("soundline2").innerHTML === "") {
 		document.getElementById("theme").value = "Pause Battle Theme";
-		document.getElementById("soundline2").innerHTML = '<audio autoplay="autoplay" hidden="hidden"><source src="theme.mp3.wav" /></audio>';
+		document.getElementById("soundline2").innerHTML = '<audio src="theme.mp3" autoplay="autoplay" hidden="hidden"></audio>';
 	} else {
 		document.getElementById("theme").value = "Play Battle Theme";
 		document.getElementById("soundline2").innerHTML = "";
