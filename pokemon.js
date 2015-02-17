1a8824c8cc91081596add8a0c7b2d30505f01fd5
Lance Lafontaine <lance.lafontaine92@gmail.com>
2015-02-17 18:32:19 -0500

    Makes number argument for playSound() function in HTML attributes more modular: allows for multiple audio sources to be included later one. Fixes CSS cursor selector. Fixes error with audio source attributes not referencing the right files. Generates a new random Cry each time the randomCry() function is invokes by making the variable local. Removes the obsoleted i variable for a previous for loop.

--- pokemon.js
+++ pokemon.js
@@ -1,18 +1,16 @@
-var randomNumber = Math.floor(Math.random()* (152 - 1)) + 1;
-var i;
-
-function playSound(soundfile) {
-	document.getElementById("soundline1").innerHTML = '<audio src="'+soundfile+'.mp3" autoplay="autoplay" hidden="hidden"></audio>';
+function playSound(number) {
+	document.getElementById("soundline1").innerHTML = '<audio src="./audio/'+number+'.mp3" autoplay="autoplay" hidden="hidden"></audio>';
 }
 
 function randomCry() {
-	document.getElementById("soundline1").innerHTML = '<audio src="'+randomNumber+'.mp3" autoplay="autoplay" hidden="hidden"></audio>';
+	var randomNumber = Math.floor(Math.random()* (152 - 1)) + 1;
+	document.getElementById("soundline1").innerHTML = '<audio src="./audio/'+randomNumber+'.mp3" autoplay="autoplay" hidden="hidden"></audio>';
 }
 
 function theme() {
 	if (document.getElementById("soundline2").innerHTML === "") {
 		document.getElementById("theme").value = "Pause Battle Theme";
-		document.getElementById("soundline2").innerHTML = '<audio src="theme.mp3" autoplay="autoplay" hidden="hidden"></audio>';
+		document.getElementById("soundline2").innerHTML = '<audio src="./audio/theme.mp3" autoplay="autoplay" hidden="hidden"></audio>';
 	} else {
 		document.getElementById("theme").value = "Play Battle Theme";
 		document.getElementById("soundline2").innerHTML = "";
